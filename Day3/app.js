166727a1f01d434587370f60dc73a9f38d820281
pxxja <poojabhambid2004@gmail.com>
2025-07-02 22:37:42 +0530

    Day 3 assignment

---
+++ Day3/app.js
@@ -0,0 +1,27 @@
+
+// greater number
+
+let num1=4;
+let num2=18;
+let num3=8;
+if(num1>num2 && num1>num3){
+    console.log(num1,"is greater");
+}else if(num2>num1 && num2>num3){
+    console.log(num2,"is greater");
+}else{
+    console.log(num3,"is greater");
+}
+
+// switch case
+let color="red"
+switch (color) {
+    case "green":
+        console.log("Avacado")
+        break;
+    case "red":
+        console.log("Apple");
+        break;
+    default:
+        console.log("Provide valid color")
+        break;
+}
