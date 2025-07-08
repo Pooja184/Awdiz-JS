d0011f6ce613273f4dbacc5cab1b1c9d6c5a999e
pxxja <poojabhambid2004@gmail.com>
2025-07-08 16:47:47 +0530

    js assignment Day7

--- Day7-arrayMethods/assignment.js
+++ Day7-arrayMethods/assignment.js
@@ -311,3 +311,272 @@ const sumOfEven=()=>{
 sumOfEven();
 
 
+console.log("-------------------Q14--------------------");
+
+//  Check if string is palindrome.
+// Input: "madam"
+// Output: true
+
+const palindrome=()=>{
+    let str="madam";
+    let i=0;
+    let j=str.length-1;
+    let isPalindrome=false;
+    while(i<j){
+      isPalindrome=false;
+      if(str[i]===str[j]){
+          isPalindrome=true;
+      }
+      i++;
+      j--;
+    }
+    if(isPalindrome){
+      console.log(true);
+    }else{
+      console.log(false);
+    }
+}
+palindrome();
+
+console.log("-------------------Q15--------------------");
+
+// Return grade based on marks using switch.
+// Input: Marks: 85
+// Output: B
+
+const gradeSystem=()=>{
+    let marks=85;
+
+    switch (true) {
+      case marks>90 && marks<=100:
+        console.log("A");
+        break;
+      case marks>80 && marks<=90:
+        console.log("B");
+        break;
+      case marks>60 && marks<=80:
+        console.log("C");
+        break;
+      case marks>40 && marks<=60:
+        console.log("D");
+        break;
+      default:
+        console.log("F")
+        break;
+    }
+}
+gradeSystem();
+
+console.log("-------------------Q16--------------------");
+
+const redeclareVar=()=>{
+    let x=10;
+    // var x=20;
+    console.log(x);
+    //output= SyntaxError: Identifier 'x' has already been declared
+}
+redeclareVar();
+
+console.log("-------------------Q17--------------------");
+// Remove duplicates from array without Set.
+// Input: [1, 2, 2, 3, 4, 1]
+// Output: [1, 2, 3, 4]
+
+const removeDuplicates=()=>{
+    let arr=[1, 2,3, 2, 3, 4, 1,4];
+    let newArr=[];
+    for(let i=0;i<arr.length;i++){
+      let isDuplicate=false;
+      for(let j=0;j<newArr.length;j++){
+        if(arr[i]===newArr[j]){
+          isDuplicate=true;
+        }
+      }
+      if(isDuplicate===false){
+        newArr[newArr.length]=arr[i]
+      }
+    }
+    console.log(newArr)
+}
+removeDuplicates();
+
+console.log("-------------------Q18--------------------");
+
+//  Slice first 3 elements of array.
+// Input: [10, 20, 30, 40, 50]
+// Output: [10, 20, 30]
+
+const sliceEle=()=>{
+    let arr=[10, 20, 30, 40, 50];
+    let newArr=arr.slice(0,3);
+    console.log(newArr)
+}
+sliceEle()
+
+console.log("-------------------Q19--------------------");
+// . Find intersection of two arrays.
+// Input: [1, 2, 3] and [2, 3, 4]
+// Output: [2, 3]
+
+const intersectionOfArray=()=>{
+    let arr1=[1, 2, 3];
+    let arr2=[2, 3, 4];
+    let newArr=[];
+    for(let i=0;i<arr1.length;i++){
+      let intersection=false;
+      for(let j=0;j<arr2.length;j++){
+        if(arr1[i]===arr2[j]){
+          intersection=true;
+        }
+      }
+      if (intersection) {
+        newArr[newArr.length]=arr1[i];
+      }
+    }
+    console.log(newArr)
+}
+intersectionOfArray();
+
+console.log("-------------------Q20--------------------");
+// Reverse array using while loop (in-place).
+// Input: [1, 2, 3, 4]
+// Output: [4, 3, 2, 1]
+
+const reverseArray=()=>{
+  let arr=[1, 2, 3, 4];
+  let i=0;
+  let j=arr.length-1
+  while (i<j) {
+    let temp=arr[i];
+    arr[i]=arr[j]
+    arr[j]=temp
+    i++;
+    j--;
+  }
+  console.log(arr);
+}
+reverseArray();
+
+console.log("-------------------Q21--------------------");
+// . Check if all elements in an array are positive (use flag variable).
+// Input: [1, 4, 6, 9]
+// Output: true
+// Use a flag set to true, and turn it false if a negative number is found in loop
+
+const checkEle=()=>{
+    let arr=[1, 4, 6, 9];
+    let isPositive=true;
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]<0){
+        isPositive=false;
+        break;
+      }
+    }
+    if(isPositive){
+      console.log(true);
+    }else{
+      console.log(false)
+    }
+}
+checkEle();
+
+console.log("-------------------Q22--------------------");
+
+// Check if a string has both uppercase and lowercase letters.
+// Input: "HelloWorld"
+// Output: true
+// Use two flags: hasUpper and hasLower.
+
+const upperLower=()=>{
+    let str='HelloWorld';
+    let hasUpper=false;
+    let hasLower=false;
+    for(let i=0;i<str.length-1;i++){
+      if(str.charCodeAt(i)>65 && str.charCodeAt(i)<91){
+        hasUpper=true;
+        // console.log(str[i])
+      }else if(str.charCodeAt(i)>97 && str.charCodeAt(i)<123){
+        hasLower=true;
+      }
+    }
+    // console.log(hasLower,hasUpper)
+    if(hasLower && hasUpper){
+      console.log(true);
+    }else{
+      console.log(false)
+    }
+}
+upperLower();
+
+console.log("-------------------Q23--------------------");
+// Determine if a number is prime using flag inside loop.
+// Input: 13
+// Output: true
+// Set flag to false if any divisor is found in loop.
+const primeUsingFlag=()=>{
+    let num=13;
+    let isPrime=true;
+    for(let i=3;i<num;i++){
+        if(num%i===0){
+          isPrime=false;
+          break;
+        }
+    }
+    if (isPrime) {
+      console.log(true);
+    }else{
+      console.log(false)
+    }
+}
+primeUsingFlag();
+
+console.log("-------------------Q24--------------------");
+// Check if an array is sorted in ascending order using flag.
+// Input: [2, 5, 8, 9]
+// Output: true
+// Set flag false if any element is greater than next.
+
+const ascending=()=>{
+    let arr=[2,5, 8, 9];
+    let hasAscending=true;
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]>arr[i+1]){
+        hasAscending=false;
+        break;
+      }
+    }
+    if (hasAscending) {
+      console.log(true);
+    }else{
+      console.log(false);
+    }
+}
+ascending();
+
+
+console.log("-------------------Q25--------------------");
+
+// Check if a word has repeating characters using flag.
+// Input: "coding"
+// Output: false
+
+const repeating=()=>{
+  let str='coding';
+  let isRepeat=false;
+  let obj={};
+  for(let i=1;i<str.length;i++){
+      if(obj[str[i]]){
+        obj[str[i]]++
+        isRepeat=true;
+      }else{
+        obj[str[i]]=1
+      }
+    }
+  console.log(obj)
+   if (isRepeat) {
+      console.log(true);
+    }else{
+      console.log(false);
+    }
+}
+repeating();
